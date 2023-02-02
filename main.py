87040f4e791c11f64df6f7c891a091792af6e434
Nick Lamicela <nupanick@gmail.com>
2023-02-01 16:18:02 -0800

    tuple communication

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python3
 
-def run_console():
-    while True:
-        text = input('> ')
-        if not text.startswith('!'): continue
-        [command, *args] = text[1:].split()
+class Bot:
+    last_printed = None
 
-        if command == 'help':
-            print('Commands: help, echo, exit')
-            continue
+    def handle(self, text):
+        # single-word commands
+
+        if text == 'exit':
+            return 'exit',
+
+        if text == 'repeat' and self.last_printed:
+            return 'print', f"I said, {self.last_printed}"
+        
+        if text == 'repeat':
+            self.last_printed = 'Nothing to repeat!'
+            return 'print', self.last_printed
+
+        if text == 'ping':
+            self.last_printed = 'pong!'
+            return 'print', self.last_printed
+
+        # multi-word commands
+        [command, *args] = text.split()
 
         if command == 'echo':
-            print(' '.join(args))
-            continue
+            self.last_printed = ' '.join(args)
+            return 'print', self.last_printed
 
-        if command == 'exit':
-            return
+        # bad command
+
+        return 'error', f"Unknown command {command}"
+
+def run_console():
+    bot = Bot()
+    while True:
+        text = input('bot> ')
+        action, *args = bot.handle(text)
+        if action == 'print':
+            print(*args)
+        if action == 'error':
+            print('[ERROR]', *args)
+        if action == 'exit':
+            break 
 
 if __name__ == '__main__': run_console()
