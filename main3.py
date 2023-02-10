ad6d87d8ccdac2f627093982db007d997095010a
Nick Lamicela <nupanick@gmail.com>
2023-02-09 16:53:11 -0800

    action arrays

--- main3.py
+++ main3.py
@@ -1,7 +1,8 @@
 class PingBot:
     def __init__(self):
         self.commands = {
-            'ping': self.ping
+            'ping': self.ping,
+            'exit': lambda _: { 'exit': True },
         }
 
     def handle(self, event):
@@ -16,7 +17,7 @@ class PingBot:
         
     def ping(self, event):
         args = event.get('args', [event.get('command')])
-        if len(args) is not 1: return {
+        if len(args) != 1: return {
             'err': f'usage: {args[0]}',
             'in': event.get('in'),
         }
@@ -25,21 +26,67 @@ class PingBot:
             'in': event.get('in'),
         }
 
+class GuessBot:
+    def __init__(self):
+        self.phase = 'idle'
+
+    def handle(self, event):
+        args = event['args']
+        command = args[0]
+                
+        if command == 'exit':
+            return [
+                { 'print': 'Goodbye!' },
+                { 'exit': True },
+            ]
+
+        if self.phase == 'idle' and command == 'start':
+            self.secret = 69
+            self.phase = 'playing'
+            return [{ 'print': "I'm thinking of a number! Try to [guess] it!" }]
+        if self.phase == 'idle':
+            return [{ 'print': 'commands: start, exit' }]
+
+        if self.phase == 'playing' and command == 'guess':
+            return self.guess(event)
+        if self.phase == 'playing' and command == 'giveup':
+            self.phase = 'idle'
+            return [{ 'print': 'game over' }]
+        if self.phase == 'playing':
+            return [{ 'print': 'commands: guess, giveup, exit' }]
+
+    def guess(self, event):
+        args = event.get('args', [])
+        if len(args) != 2:
+            return [{ 'print': f'usage: {args[0]} <number>' }]
+
 def run_console():
-    bot = PingBot()
+    bot = GuessBot()
+    actions = []
     while True:
-        text = input('> ')
-        if not text.startswith('!'): continue
-        args = text[1:].split()
-        event = {
-            'text': text,
-            'args': args,
-            'command': args[0],
-            'from': 'console',
-            'in': 'console',
-        }
-        action = bot.send(event)
-        if action.get('print'):
-            if action.get('in') == 'console':
-                print(f"<bot> ")
-        
+        if not actions:
+            text = input('<user> ')
+            args = text.split()
+            command = args[0]
+            event = {
+                'text': text,
+                'args': args,
+                'command': command,
+                'from': 'user',
+                'room': 'console',
+            }
+            try:
+                actions = bot.handle(event) or []
+                continue
+            except Exception as e:
+                print(f'[ERROR]: {e}')
+                continue
+        a = actions.pop(0)
+        if 'error' in a: 
+            print(f"[ERROR]: {a['error']}")
+        if 'print' in a:
+            print(f"<bot> {a['print']}")     
+        if 'exit' in a:
+            break   
+
+if __name__ == '__main__': run_console()
