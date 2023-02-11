e67dfbc7b385032b0a7aa7403921866c679fe08e
Nick Lamicela <nupanick@gmail.com>
2023-02-10 17:52:34 -0800

    nice

--- main3.py
+++ main3.py
@@ -1,39 +1,11 @@
-class PingBot:
-    def __init__(self):
-        self.commands = {
-            'ping': self.ping,
-            'exit': lambda _: { 'exit': True },
-        }
-
-    def handle(self, event):
-        command = event.get('command')
-        func = self.commands.get(command)
-        if func:
-            return func(event)
-        return {
-            'err': f'{type(self).__name__} cannot {command}',
-            'in': event.get('in'),
-        }
-        
-    def ping(self, event):
-        args = event.get('args', [event.get('command')])
-        if len(args) != 1: return {
-            'err': f'usage: {args[0]}',
-            'in': event.get('in'),
-        }
-        return {
-            'print': 'pong!',
-            'in': event.get('in'),
-        }
-
-class GuessBot:
+class GuessingGame:
     def __init__(self):
         self.phase = 'idle'
+        self.commands = { 'start', 'exit' }
 
     def handle(self, event):
-        args = event['args']
-        command = args[0]
-                
+        command = event['args'][0]
+
         if command == 'exit':
             return [
                 { 'print': 'Goodbye!' },
@@ -41,52 +13,63 @@ class GuessBot:
             ]
 
         if self.phase == 'idle' and command == 'start':
-            self.secret = 69
+            self.secret = 69 # nice
             self.phase = 'playing'
             return [{ 'print': "I'm thinking of a number! Try to [guess] it!" }]
+        
         if self.phase == 'idle':
             return [{ 'print': 'commands: start, exit' }]
 
         if self.phase == 'playing' and command == 'guess':
             return self.guess(event)
+        
         if self.phase == 'playing' and command == 'giveup':
             self.phase = 'idle'
-            return [{ 'print': 'game over' }]
+            return [{ 'print': f'No worries! I was thinking of {self.secret}.' }]
+        
         if self.phase == 'playing':
             return [{ 'print': 'commands: guess, giveup, exit' }]
 
+        return [{ 'error': f'unable to handle command {command} in phase {self.phase}.' }]
+
     def guess(self, event):
+        # make sure the user typed a number
         args = event.get('args', [])
-        if len(args) != 2:
+        try:
+            assert len(args) == 2
+            num = int(args[1])
+        except:
             return [{ 'print': f'usage: {args[0]} <number>' }]
 
+        if num < self.secret:
+            return [{ 'print': 'Too low!' }]
+        elif num > self.secret:
+            return [{ 'print': 'Too high!' }]
+        else:
+            self.phase = 'idle'
+            return [{ 'print': 'Yep, that was it! Nice.' }]
+
 def run_console():
-    bot = GuessBot()
+    bot = GuessingGame()
     actions = []
     while True:
+        
         if not actions:
             text = input('<user> ')
             args = text.split()
-            command = args[0]
-            event = {
-                'text': text,
-                'args': args,
-                'command': command,
-                'from': 'user',
-                'room': 'console',
-            }
+            event = { 'text': text, 'args': args }
             try:
-                actions = bot.handle(event) or []
-                continue
+                actions = bot.handle(event)
             except Exception as e:
                 print(f'[ERROR]: {e}')
-                continue
+            continue
+
         a = actions.pop(0)
         if 'error' in a: 
             print(f"[ERROR]: {a['error']}")
         if 'print' in a:
             print(f"<bot> {a['print']}")     
         if 'exit' in a:
-            break   
+            break
 
 if __name__ == '__main__': run_console()
